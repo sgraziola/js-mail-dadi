6bff18d1f473065cc9cad095f006d33833222591
sgraziola <simo.fdl@gmail.com>
2022-10-11 17:43:14 +0200

    Ex Mail&Dice: remove break, add isValid var

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,19 @@ const emailElement = prompt("Inserisci la tua email");
     //-creo array di email 
 const validEmails = ["dimaio@gmail.com", "berlusconi@tiscali.it", "prodi@virgilio.it", "letta@gmail.com", "meloni@libero.it", "salvini@fastwebnet.it"];
     //-confronto emailElement con i singoli indici dell'array
+let isValid = "Mi dispiace, non fai parte della fantastica classe politica italiana";
+
 for (let i = 0; i < validEmails.length; i++){
     const validEmailEl = validEmails[i];
     //console.log(validEmailEl);
     //stampa un messaggio appropriato sull’esito del controllo. 
         //- If/else a seconda che l'email compaia o meno creo un messaggio
     if(emailElement === validEmailEl){
-        console.log("Complimenti! Sei uno dei nostri fantastici politici.");
-        break;
-    } else {
-        console.log("Mi dispiace, non fai parte della fantastica classe politica italiana");
+        isValid = "Complimenti! Sei uno dei nostri fantastici politici.";   
     }
 }
+console.log(isValid);
+
 
 /* Soluzione alternativa con .indexof() */
 /* let index = validEmails.indexOf(emailElement);
